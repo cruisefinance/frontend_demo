8fc9373ffbed9820bccf36e727dcac0243c98b94
dinosaurfinance <dinosaur.finance@protonmail.com>
2021-06-05 17:14:27 +0200

    init

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,79 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://quickswap.exchange/#/swap?outputCurrency=0xaD04791c59547CCd826A164FFB9061Cf12b93fD4',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://quickswap.exchange/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: '/pools',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'QuickSwap',
+        href: 'https://info.quickswap.exchange/token/0xaD04791c59547CCd826A164FFB9061Cf12b93fD4',
+      },
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/PolySwanFinance',
+      },
+      {
+        label: 'Docs',
+        href: 'https://polyswan-finance.gitbook.io/polyswan-finance/',
+      },
+    ],
+  },
+  {
+    label: 'Audit (SOON)',
+    icon: 'AuditIcon',
+    href: 'https://polyswan.finance/',
+  },
+]
+
+export default config
---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,31 @@
+export default {
+  cake: {
+    137: '0xaD04791c59547CCd826A164FFB9061Cf12b93fD4'
+  },
+  masterChef: {
+    137: '0xA12005edA0eb0f83462D39A8452b3b976f926a76',
+  },
+  weth: {
+    137: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619"
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    137: '0x40524425bcb58Dc16f11746B0028e23aCDA1e128'
+  },
+  usdc: {
+    137: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
+  },
+  wmatic: {
+    137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+  },
+  matic: {
+    137: '0x0000000000000000000000000000000000001010'
+  }
+}
