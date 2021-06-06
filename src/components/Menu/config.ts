47f929d35d3170072ca01e62884288a282d23103
Fleymar <56514321+Fleymar@users.noreply.github.com>
2021-06-06 03:01:31 +0200

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -43,7 +43,7 @@ const config: MenuEntry[] = [
     {
     label: "T-Rex's Pantry (SOON)",
     icon: 'VaultsIcon',
-    href: 'https://dinosaur-frontend.vercel.app/'
+    href: '/vaults'
     },
     {
     label: 'Eggs Incubator (SOON)',
